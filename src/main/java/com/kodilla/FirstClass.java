f6245ee2e864e24b9d7ccfe33369f6a468653f1d
“mazureq <poczta.mazur@gmail.com>
2018-05-13 23:22:32 +0200

    Work with mentor

--- src/main/java/com/kodilla/FirstClass.java
+++ src/main/java/com/kodilla/FirstClass.java
@@ -4,5 +4,7 @@ public class FirstClass {
 
     public static void main(String[] args) {
         System.out.println("Hello");
+        System.out.println("aa");
+        System.out.println("bbb");
     }
 }
