263492c3429277a53857bbddf816604f3b05a84c
“mazureq <poczta.mazur@gmail.com>
2018-05-17 23:10:52 +0200

    Junit Test extermination odd numbers

--- kodilla-testing/src/main/java/com/kodilla/testing/collection/OddNumbersExterminator.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/collection/OddNumbersExterminator.java
@@ -1,4 +1,19 @@
 package com.kodilla.testing.collection;
 
+import java.util.ArrayList;
+
 public class OddNumbersExterminator {
+
+
+    ArrayList<Integer>evenList = new ArrayList<>();
+    public void exterminate(ArrayList<Integer> numbers) {
+
+        for (Integer theNumber: numbers) {
+            int temporary = theNumber % 2;
+            if (temporary == 0){
+                evenList.add(theNumber);
+                System.out.println(theNumber);
+            }
+        }
+    }
 }
---
+++ kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
@@ -0,0 +1,41 @@
+package com.kodilla.testing.collection;
+import org.junit.*;
+import com.kodilla.testing.collection.OddNumbersExterminator;
+
+import java.util.ArrayList;
+
+
+public class CollectionTestSuite {
+    @Before
+    public void before(){
+        System.out.println("Case is in test");
+    }
+    @After
+    public void after(){
+        System.out.println("Case has been tested");
+    }
+
+    @Test
+
+    public void testOddNumbersExterminatorEmptyList() {
+        ArrayList<Integer> theEmptyList = new ArrayList<>();
+        OddNumbersExterminator exterminator1 = new OddNumbersExterminator();
+        exterminator1.exterminate(theEmptyList);
+    }
+
+
+    @Test
+
+    public void testOddNumbersExterminatorNormalList() {
+        ArrayList<Integer> theNormalList = new ArrayList<>();
+        for (int i =0; i < 10; i++){
+            theNormalList.add(i + 1);
+        }
+
+        OddNumbersExterminator exterminator2 = new OddNumbersExterminator();
+        exterminator2.exterminate(theNormalList);
+
+    }
+
+
+}
