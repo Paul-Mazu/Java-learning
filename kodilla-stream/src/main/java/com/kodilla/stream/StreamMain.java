52988600b64025c6e574102fe96d07b5bccb72d9
“mazureq <poczta.mazur@gmail.com>
2018-06-01 20:41:20 +0200

    Stream Unmutable Class - task 7.2

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,6 +1,7 @@
 package com.kodilla.stream;
 
         import com.kodilla.stream.beautifier.PoemBeautifier;
+        import com.kodilla.stream.iterate.NumbersGenerator;
         import com.kodilla.stream.lambda.ExpressionExecutor;
         import com.kodilla.stream.reference.FunctionalCalculator;
 
@@ -25,8 +26,10 @@ public class StreamMain {
         poemBeautifier.beautify("Text", (anyString) -> "!!! " + anyString + " !!!");
         poemBeautifier.beautify("Text", (anyString) -> anyString.toUpperCase());
         poemBeautifier.beautify("Text", (anyString) -> anyString.toLowerCase());
+        System.out.println();
 
-
+        System.out.println("Ising Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
 
     }
 }
