6b9be97db0855f40d14167e48c3899b17bfd4426
“mazureq <poczta.mazur@gmail.com>
2018-07-02 23:11:02 +0200

    Exceptions - task 8.3

--- kodilla-exception/src/main/java/com/kodilla/exception/io/FileReaderWithoutHandling.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/io/FileReaderWithoutHandling.java
@@ -7,24 +7,15 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.stream.Stream;
 
-public class FileReader {
+public class FileReaderWithoutHandling {
 
-    public void readFile() throws FileReaderException {
+    public void readFile() throws IOException {
 
         ClassLoader classLoader = getClass().getClassLoader();
         File file = new File(classLoader.getResource("file/names.txt").getFile());
         Path path = Paths.get(file.getPath());
+        Stream<String> fileLines = Files.lines(path);
+        fileLines.forEach(System.out::println);
 
-        try (Stream<String> fileLines = Files.lines(path);) {
-
-            fileLines.forEach(System.out::println);
-
-        } catch (IOException e) {
-
-            throw new FileReaderException();
-
-        } finally {
-            System.out.println("I will always be there");
-        }
     }
 }
