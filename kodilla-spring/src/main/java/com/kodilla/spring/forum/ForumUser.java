3eb64a7089240608a73f98490f12fcdbb6b0dbe0
“mazureq <poczta.mazur@gmail.com>
2018-07-23 22:36:24 +0200

    Spring first test - task 10.1

---
+++ kodilla-spring/src/main/java/com/kodilla/spring/forum/ForumUser.java
@@ -0,0 +1,14 @@
+package com.kodilla.spring.forum;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class ForumUser {
+
+
+    String forumUser;
+
+    public ForumUser() {
+        forumUser = "John Smith";
+    }
+}
