c489833eec58c9418807cfa169f6beffd04138fd
“mazureq <poczta.mazur@gmail.com>
2018-07-24 22:10:28 +0200

    Spring BeansConfiguration - task 10.2

---
+++ kodilla-spring/src/test/java/com/kodilla/spring/Library/LibraryTestSuit.java
@@ -0,0 +1,36 @@
+package com.kodilla.spring.Library;
+
+import com.kodilla.spring.library.Library;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LibraryTestSuit {
+
+    @Test
+    public void testLoadFromDb() {
+        //Given
+        ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring");
+        Library library = context.getBean(Library.class);
+        //When
+        library.loadFrom();
+        //Then
+        //Do nothing
+    }
+
+    @Test
+    public void testSaveToDb() {
+        //Given
+        ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring");
+        Library library = context.getBean(Library.class);
+        //When
+        library.saveDoDb();
+        //Then
+        //Do nothing
+    }
+}
---
+++ kodilla-spring/src/test/java/com/kodilla/spring/calculator/CalculatorTestSuit.java
@@ -0,0 +1,26 @@
+package com.kodilla.spring.calculator;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CalculatorTestSuit {
+    @Test
+    public void testCalculations() {
+        //Given
+        ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring");
+        Calculator calculator = context.getBean(Calculator.class);
+        //When
+        calculator.add(10,10);
+        calculator.sub(15, 10);
+        calculator.mul(2, 2);
+        calculator.div(10, 5);
+        //Then
+        //Do Nothing
+    }
+}
